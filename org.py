4c0ef06618b5a44c9c2ba88377a8c6ebd9245f89
honmaple <xiyang0807@gmail.com>
2017-03-17 09:36:09 +0800

    update

--- org.py
+++ org.py
@@ -6,7 +6,7 @@
 # Author: jianglin
 # Email: xiyang0807@gmail.com
 # Created: 2017-03-15 14:48:01 (CST)
-# Last Update:星期四 2017-3-16 16:45:37 (CST)
+# Last Update:星期五 2017-3-17 9:35:58 (CST)
 #          By:
 # Description:
 # **************************************************************************
@@ -15,7 +15,7 @@ from html import escape
 
 
 class Regex(object):
-    header = re.compile(r'^(?P<level>\*+)\s+(?P<title>.+)$')
+    heading = re.compile(r'^(?P<level>\*+)\s+(?P<title>.+)$')
     bold = re.compile(r'\*(?P<text>[\S]+?)\*')
     # italic = re.compile(r'(\*\*|/)(?P<text>[\S]+?)(\*\*|/)')
     italic = re.compile(r'\*\*(?P<text>[\S]+?)\*\*')
@@ -26,14 +26,14 @@ class Regex(object):
     image = re.compile(r'\[\[(?P<src>.+?)\](?:\[(?P<alt>.+?)\])?\]')
     link = re.compile(r'\[\[(?P<href>https?://.+?)\](?:\[(?P<text>.+?)\])?\]')
 
-    begin_example = re.compile(r'^#\+BEGIN_EXAMPLE$')
-    end_example = re.compile(r'^#\+END_EXAMPLE$')
+    begin_example = re.compile(r'\s*#\+BEGIN_EXAMPLE$')
+    end_example = re.compile(r'\s*#\+END_EXAMPLE$')
 
-    begin_quote = re.compile(r'^#\+BEGIN_QUOTE$')
-    end_quote = re.compile(r'^#\+END_QUOTE$')
+    begin_quote = re.compile(r'\s*#\+BEGIN_QUOTE$')
+    end_quote = re.compile(r'\s*#\+END_QUOTE$')
 
-    begin_src = re.compile(r'#\+BEGIN_SRC\s*(?P<lang>.+)$')
-    end_src = re.compile(r'^#\+END_SRC$')
+    begin_src = re.compile(r'\s*#\+BEGIN_SRC\s*(?P<lang>.+)$')
+    end_src = re.compile(r'\s*#\+END_SRC$')
 
     order_list = re.compile(r'(?P<depth>\s*)\d+(\.|\))\s+(?P<item>.+)$')
     unorder_list = re.compile(r'(?P<depth>\s*)(-|\+)\s+(?P<item>.+)$')
@@ -46,19 +46,7 @@ class Element(object):
         self.regex = regex
 
 
-class Node(object):
-    label = ''
-
-    def __init__(self, text):
-        self.text = text
-
-    @property
-    def html(self):
-        return '<{label}>{text}</{label}>'.format(
-            label=self.label, text=self.text)
-
-
-class Header(Element):
+class Heading(Element):
     def __init__(self, regex, offset=1):
         self.offset = offset
         self.regex = regex
@@ -67,8 +55,7 @@ class Header(Element):
         m = self.regex.match(text)
         level = len(m.group('level')) + (self.offset - 1)
         title = m.group('title')
-        return '<h{level}>{title}</h{level}>'.format(
-            level=level, title=title)
+        return '<h{level}>{title}</h{level}>'.format(level=level, title=title)
 
 
 class Table(Element):
@@ -238,16 +225,16 @@ class Text(object):
             text = Image(Regex.image).parse(text)
         return text
 
-    @property
-    def html(self):
+    def to_html(self):
         return self._html
 
 
 class OrgMode(object):
-    def __init__(self):
+    def __init__(self, offset=1):
+        self.heading_offset = offset
         self.regex = Regex
         self._html = ''
-        self._headers = []
+        self._headings = []
         self._example_flag = False
         self._quote_flag = False
         self._src_flag = False
@@ -291,7 +278,6 @@ class OrgMode(object):
     def parse(self, text):
         text = text.splitlines()
         for line in text:
-            line = escape(line.strip(), True)
             if Regex.unorder_list.match(line):
                 self.parse_unorder_list(line)
             elif Regex.order_list.match(line):
@@ -309,8 +295,8 @@ class OrgMode(object):
             self.parse_quote(text)
         elif self._src_flag:
             self.parse_src(text)
-        elif Regex.header.match(text):
-            self.parse_header(text)
+        elif Regex.heading.match(text):
+            self.parse_heading(text)
         elif Regex.begin_example.match(text):
             self._example_flag = True
         elif Regex.begin_quote.match(text):
@@ -319,8 +305,8 @@ class OrgMode(object):
             self._src_flag = True
             self._src_lang = Regex.begin_src.match(text).group('lang')
         else:
-            text = Text(text).html
-            self._html += text
+            text = Text(text).to_html()
+            self.current.append(text)
 
     def parse_unorder_list(self, text):
         l = UnorderList(Regex.unorder_list, self._unorder_list_depth)
@@ -352,10 +338,10 @@ class OrgMode(object):
         self.elements.append(table)
         self.current.append(table.parse(text))
 
-    def parse_header(self, text):
-        header = Header(Regex.header)
-        self.elements.append(header)
-        self._html += header.parse(text)
+    def parse_heading(self, text):
+        heading = Heading(Regex.heading, self.heading_offset)
+        self.elements.append(heading)
+        self.current.append(heading.parse(text))
 
     def parse_example(self, text):
         if Regex.end_example.match(text):
@@ -366,7 +352,7 @@ class OrgMode(object):
             self._example_flag = False
             self._begin_buffer = ''
         else:
-            self._begin_buffer += text
+            self._begin_buffer = text + '\n'
 
     def parse_src(self, text):
         if Regex.end_src.match(text):
@@ -377,7 +363,7 @@ class OrgMode(object):
             self._src_flag = False
             self._begin_buffer = ''
         else:
-            self._begin_buffer += text
+            self._begin_buffer = text + '\n'
 
     def parse_quote(self, text):
         if Regex.end_quote.match(text):
@@ -389,17 +375,22 @@ class OrgMode(object):
             self._begin_buffer = ''
         else:
             text = Text(text).html
-            self._begin_buffer += text
+            self._begin_buffer = text + '\n'
 
     def append(self, text):
-        self._html += text
+        if self._src_flag or self._example_flag:
+            text = text.rstrip() + '\n'
+            self._html += text
+        elif text:
+            text = text.strip() + '\n'
+            self._html += text
 
     def to_html(self):
         self.close()
         return self._html
 
 
-def orgmode(text):
-    org = OrgMode()
+def org_to_html(text, heading_offset=1):
+    org = OrgMode(heading_offset)
     org.parse(text)
     return org
